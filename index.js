96380c21b49976c17ed7c99cc960df195bcd1304
yas lillo <yasnalillo27@gmail.com>
2022-01-15 12:52:44 -0300

    puerto

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
